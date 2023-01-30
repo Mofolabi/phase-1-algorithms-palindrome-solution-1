159fe0efdaeba9e10c1a8687d71a9f84eec88703
Mofolabi Osotimehin <mofolabi.osotimehin@student.moringaschool.com>
2023-01-30 15:39:07 +0100

    Done with Assignment

--- index.js
+++ index.js
@@ -23,3 +23,20 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+
+function reverseString(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
